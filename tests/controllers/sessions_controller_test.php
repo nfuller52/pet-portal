0f662788253149a7be6aeb678fbf1cfdcd6ae662
Nick <nfuller52@gmail.com>
2016-01-24 00:22:52 -0800

    create a sessions controller test stub

---
+++ tests/controllers/sessions_controller_test.php
@@ -0,0 +1,11 @@
+<?php
+
+class SessionsControllerTest extends WP_UnitTestCase {
+
+	function test_sample() {
+		// replace this with some actual testing code
+		$this->assertTrue( true );
+	}
+
+}
+
