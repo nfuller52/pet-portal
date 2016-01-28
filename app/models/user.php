b3eb3cbf15bd67c4730ab004cf2618d0bc847d9d
Nick <nfuller52@gmail.com>
2016-01-28 11:25:56 -0800

    Create the authentication method on the User object

--- app/models/user.php
+++ app/models/user.php
@@ -1,6 +1,33 @@
 <?php
 namespace PetPortal\App\Models;
 
+use PetPortal\App\Models\Apollo;
+use PetPortal\App\Models\Users\Login;
+use PetPortal\Lib\Error;
+
 class User {
+
+	public function __construct( $apollo = null )
+	{
+
+		if ( $apollo === null ) { $apollo = new Apollo(); }
+
+		$this->apollo = $apollo;
+
+	}
+
+	public function authenticate( $username, $password )
+	{
+
+		$response = $this->apollo->access_token( $username, $password );
+
+		if ( is_wp_error( $response ) ) {
+			return Error::cannot_connect_to_auth_server();
+		} else {
+			$login = new Login();
+			return $login->user( $username, $response );
+		}
+
+	}
 
 }
