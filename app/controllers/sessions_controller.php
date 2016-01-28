f7bd9565518e3bbdfb06e59fbfc94037dae5c182
Nick <nfuller52@gmail.com>
2016-01-28 11:24:44 -0800

    Create the sessions controller create method and call it when the plugin loads

--- app/controllers/sessions_controller.php
+++ app/controllers/sessions_controller.php
@@ -2,15 +2,30 @@
 namespace PetPortal\App\Controllers;
 
 use PetPortal\App\Controllers\ApplicationController;
+use PetPortal\App\Models\User;
 
 class SessionsController extends ApplicationController {
 
-	/*
-	 * Sign in
+	/**
+	 * Log in a user and start a WordPress session
 	 *
+	 * @param  WP_User $user    The WordPress user object passed through the action
+	 * @param  string $username The username passed through the WordPress form
+	 * @param  string $password The password passed through the WordPress form
+	 * @return WP_User|false    Returns the user to login or false with incorrect credentials
 	 */
-	public function create()
+	public function create( $user, $username, $password )
 	{
+
+		remove_action( 'authenticate', 'wp_authenticate_username_password', 20 );
+
+		if ( $this->user_can_login( $username, $password ) ) {
+			$user = new User();
+			return $user->authenticate( $username, $password );
+		} else {
+			add_filter( 'login_errors', create_function( '$a', 'return "Please enter your username and password.";' ) );
+			return false;
+		}
 
 	}
 
@@ -20,6 +35,27 @@ class SessionsController extends ApplicationController {
 	 */
 	public function destroy()
 	{
+
+	}
+
+	// Private methods
+	/////////////////////////////////////////////
+
+	/**
+	 * Determine if the username and password have been set
+	 *
+	 * @param  string $username The username provided via the WordPress form
+	 * @param  string $password The password provided via the WordPress form
+	 * @return bool
+	 */
+	private function user_can_login( $username, $password )
+	{
+
+		return ! (
+			( trim( $username ) === '' || $username === null )
+			||
+			( trim( $password ) === '' || $password === null )
+		);
 
 	}
 
